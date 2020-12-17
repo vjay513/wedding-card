f1a537711d8ea745ef5d071a31d16b37c8394783
Vijay Bammidi <Vijay_Bammidi@epam.com>
2020-12-17 18:33:02 +0530

    logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,39 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-
+  days:any;
+  hours:any;
+  minutes:any;
+  seconds:any;
   ngOnInit(){
     const second = 1000,
     minute = second * 60,
     hour = minute * 60,
     day = hour * 24;
 
-let birthday = "jan 06, 2021 00:00:00",
-  countDown = new Date(birthday).getTime(),
-  x = setInterval(function() {    
-
-    let now = new Date().getTime(),
-        distance = countDown - now;
-
-    document.getElementById("days").innerText = Math.floor(distance / (day)),
-      document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-      document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-      document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-    //do something later when date is reached
-    if (distance < 0) {
-      let headline = document.getElementById("headline"),
-          countdown = document.getElementById("countdown"),
-          content = document.getElementById("content");
-
-      headline.innerText = "It's my birthday!";
-      countdown.style.display = "none";
-      content.style.display = "block";
-
-      clearInterval(x);
-    }
-    //seconds
+  let marriage = "jan 06, 2021 00:00:00",
+  countDown = new Date(marriage).getTime(),
+  x = setInterval(() =>{    
+      let now = new Date().getTime(),
+      distance = countDown - now;
+      this.days = Math.floor(distance / (day)),
+      this.hours = Math.floor((distance % (day)) / (hour)),
+      this.minutes = Math.floor((distance % (hour)) / (minute)),
+      this.seconds = Math.floor((distance % (minute)) / second);
   }, 0)
   }
-
 }
